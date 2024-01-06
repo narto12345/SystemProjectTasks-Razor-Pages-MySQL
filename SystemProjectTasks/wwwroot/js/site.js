92ea6fa21a5f6aa244a526ba80895dc692b1e3f9
Nicolas Santiago Sosa Jimenez <nicosan12@hotmail.com>
2024-01-05 19:55:09 -0500

    Add files via upload

---
+++ SystemProjectTasks/wwwroot/js/site.js
@@ -0,0 +1,24 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+let tdDescription = document.getElementsByClassName("description");
+const tablaProyectos = document.getElementById("tabla-proyectos");
+const tablaCrearProyectos = document.getElementById("formulario-creacion");
+let botonCrear = false;
+
+for (const iterator of tdDescription) {
+  iterator.setAttribute("title", iterator.innerText);
+}
+
+const boton = document.getElementById("boton");
+boton.addEventListener("click", () => {
+  tablaProyectos.classList.toggle("oculto");
+  tablaCrearProyectos.classList.toggle("oculto");
+  botonCrear = !botonCrear;
+  if (botonCrear) {
+    boton.innerText = "Mostrar Proyectos";
+  } else {
+    boton.innerText = "Crear Proyecto";
+  }
+});
